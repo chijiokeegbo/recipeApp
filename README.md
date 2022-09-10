e11e114907ef5f2d3a6705a2fd40377240a5fe7c
Chijioke Egbo <chijioke.egbo>
2022-09-10 15:47:34 +0100

    slight upgrade

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # recipeApp
 recipe app using vanilla javascript an application that will enable eateries and restaurant to recieve order placement over the internet.
+ This will enable small business owners and big business owners to offer proper service to their customers
--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const favoriteContainer = document.getElementById("fav-meals");
 const searchTerm = document.getElementById("search-term");
 const searchBtn = document.getElementById("search");
 const mealPopup = document.getElementById("meal-popup");
+const mealInfoEL = document.getElementById("meal-info");
 const popupCloseBtn = document.getElementById("close-popup");
 
 getRandomMeal();
@@ -62,7 +63,10 @@ function addMeal(mealData, random = false){
                 }
                 fetchFavMeals();
             });
-                
+            
+            meal.addEventListener("click", () => {
+                showMealInfo(mealData);
+            })
             mealEL.appendChild(meal);
 }
 
@@ -101,19 +105,55 @@ async function fetchFavMeals(){
 
 function addMealFav(mealData){
     const favMeal = document.createElement("li");
+    
     favMeal.innerHTML = `<img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
             <span>${mealData.strMeal}</span>
             <button class="clear"><i class="fas fa-window-close"></i></button>
             `;
 
             const btn = favMeal.querySelector(".clear");
+
             btn.addEventListener("click", () => {
                 removeMealLS(mealData.idMeal);
 
                 fetchFavMeals();
             })
-
+            favMeal.addEventListener("click", () => {
+            showMealInfo(mealData)
+        })
+        
         favoriteContainer.appendChild(favMeal);
+}
+
+function showMealInfo(mealData){
+    mealInfoEL.innerHTML = "";
+    const mealEl = document.createElement('div');
+    
+    const ingredients = [];
+    for(let i = 0; i <= 20; i++){
+        if(mealData['strIngredient'+i]){
+            ingredients.push(`${mealData['strIngredient'+i]} - ${mealData['strMeasure'+i]}`);
+            console.log(ingredients)
+        }else{
+            break;
+        }
+        
+    }
+
+    mealEl.innerHTML =  `<h1>${mealData.strMeal}</h1>
+                        <img src="${mealData.strMealThumb}" alt="${meal.strMeal}">
+                        <p>
+                        ${mealData.strInstructions}
+                        </p>
+                        <h3>Ingredient:</h3>
+                        <ul>
+                            ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+                        </ul>
+                    `;
+
+    mealInfoEL.appendChild(mealEl);
+    mealPopup.classList.remove('hidden');
+   
 }
 
 searchBtn.addEventListener("click", async () => {
